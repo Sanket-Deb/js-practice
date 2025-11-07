69db85b3674990ca1b795c0d010d08b65724a3b3
Sanket Deb <imsanketdeb20@gmail.com>
2025-11-07 21:06:33 +0530

    new q4 update

--- questions/Q4_tobeornotbe.js
+++ questions/Q4_tobeornotbe.js
@@ -3,9 +3,37 @@
 
 const q4_tobeornotbe = (input) => {
   // TODO: implement solution
-  return input;
+  const toBe = (val) => {
+    if (val === input) {
+      return true;
+    } else throw new Error("Not Equal");
+  };
+
+  const notToBe = (val) => {
+    if (val !== input) {
+      return true;
+    } else throw new Error("Equal");
+  };
+
+  return { toBe, notToBe };
 };
 
-console.log(q4_tobeornotbe('example input'));
+const tobeornotbe = q4_tobeornotbe(5);
+
+const safeCall = (fn) => {
+  try {
+    return fn();
+  } catch (error) {
+    return error.message;
+  }
+};
+const output = [
+  safeCall(() => tobeornotbe.toBe(5)),
+  safeCall(() => tobeornotbe.toBe(4)),
+  safeCall(() => tobeornotbe.notToBe(5)),
+  safeCall(() => tobeornotbe.notToBe(6)),
+];
+
+console.log(output);
 
 export default q4_tobeornotbe;
--- questions/Q4_tobeornotbe.md
+++ questions/Q4_tobeornotbe.md
@@ -1,12 +1,18 @@
 ### Q4_tobeornotbe: toBeorNotBe
 
 **Prompt:**  
-Describe the problem statement here.
+THIS QUESTION WAS PART OF LEETCODE 30-DAYS JS CHALLENGE.
 
-**Example:**  
+Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+**Example:**
+
 ```
-Input: 
-Output:
+Input: func = () => expect(5).toBe(5)
+Output: {"value": true}
 ```
 
 **Approach:**  
