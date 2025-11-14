2da07e5c95488039368e5b2f05b2fde0ecd3bf26
Sanket Deb <imsanketdeb20@gmail.com>
2025-11-14 23:20:04 +0530

    memoization file created

---
+++ questions/Q10_memoize.js
@@ -0,0 +1,11 @@
+// Q10_memoize.js
+// Boilerplate for: memoize
+
+const q10_memoize = (input) => {
+  // TODO: implement solution
+  return input;
+};
+
+console.log(q10_memoize('example input'));
+
+export default q10_memoize;
---
+++ questions/Q10_memoize.md
@@ -0,0 +1,13 @@
+### Q10_memoize: memoize
+
+**Prompt:**  
+Describe the problem statement here.
+
+**Example:**  
+```
+Input: 
+Output:
+```
+
+**Approach:**  
+Explain your thought process here.
